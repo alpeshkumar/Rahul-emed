69f30fee60ed3913a5932350a5fd0d7a29a5e7d7
Rahul <Rahulpat.rp@Gmail.com>
2021-03-23 13:44:12 +0530

    23rd march html updated
    9 pages html created and home page responsive done other page responsive working

--- wp-content/themes/eMed/inc/assets/js/script.js
+++ wp-content/themes/eMed/inc/assets/js/script.js
@@ -6,11 +6,23 @@ jQuery(document).ready(function() {
         prevArrow: false,
         nextArrow: false,
         dots: true,
-        slidesToScroll: 1
-            // asNavFor: '.slider-for',
-
-        // dots: true,
-        // focusOnSelect: true
+        slidesToScroll: 1,
+        responsive: [{
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 2,
+                    adaptiveHeight: true,
+                },
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                },
+            },
+        ],
     });
     jQuery('.slider-digital').slick({
         slidesToShow: 5,
@@ -18,6 +30,22 @@ jQuery(document).ready(function() {
         prevArrow: false,
         nextArrow: false,
         dots: true,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        responsive: [{
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 2,
+                    adaptiveHeight: true,
+                },
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                },
+            },
+        ],
     });
 });
